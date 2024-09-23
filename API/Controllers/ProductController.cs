555e2cabfd80c66221d0adfa06910c1b6315675e
ganeshkrishna217 <ganeshbunny57@gmail.com>
2024-09-23 15:50:18 +0530

    end of phase 1

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProductController(DataContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            return await _context.Products.ToListAsync();
+            
+        }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Product>> GetProduct(int Id){
+            return await _context.Products.FindAsync(Id);
+        }
+    }
+}
